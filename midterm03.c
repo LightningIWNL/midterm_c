24551f830be42ddf97278de31a616eaf719fab22
LightningIWNL <flukzfl@gmail.com>
2023-07-06 15:27:50 +0700

    fixed and complete

--- midterm03.c
+++ midterm03.c
@@ -7,7 +7,7 @@ void main(){
     line
     printf("Input Celcius: ");
     scanf("%f",&celc);
-    farh = celc - 32 *5/9;
+    farh = celc * 9/5 +32;
     line
     printf("Farenheit is: %.2f\n",farh);
     line
